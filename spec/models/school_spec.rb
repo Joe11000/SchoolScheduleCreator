9ca4a09734cf6b58f33d551670129c7c9839d311
Joe <JoeNoonan27@gmail.com>
2013-12-08 19:58:09 -0600

    Updated School

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,39 +1,47 @@
 require 'spec_helper'
 
 describe School do
-	before(:all) do
-		@school = FactoryGirl.build(:school)
-		@school.password              = "1234"
-		@school.password_confirmation = "1234"
+  before(:all) do
+    @school = FactoryGirl.build(:school)
+    @school.password              = "1234"
+    @school.password_confirmation = "1234"
     @school.save()
 
     @teacher = FactoryGirl.create(:teacher)
      @school.teachers << @teacher
-	end
+  end
+
+  after(:all) do
+    @school.destroy
+    @teacher.destroy
+  end
 
   it "has a name" do
-  	expect(@school.name).to eq "SchoolA"
+    expect(@school.name).to eq "SchoolA"
   end
 
   it "has a location" do
-		expect(@school.location).to eq "32124.12345 886.4366743256"
+    expect(@school.location).to eq "32124.12345 886.4366743256"
   end
 
   it "has a password" do
-		expect(@school.authenticate('1234')).to be_true
+    expect(@school.authenticate('1234')).to be_true
   end
 
   it "has a bio" do
-  	expect(@school.bio).to eq "Opened Last Thursday."
+    expect(@school.bio).to eq "Opened Last Thursday."
   end
 
   it { should have_secure_password }
+
   it { should allow_mass_assignment_of(:bio) }
   it { should allow_mass_assignment_of(:location) }
   it { should allow_mass_assignment_of(:name) }
+
   it { should_not allow_mass_assignment_of(:password) }
 
+  it { should validate_uniqueness_of(:name) }
   it { should have_many(:teachers) }
-  it { should have_many(:courses_pools) }
+  it { should have_many(:courses_pools).dependent(:destroy) }
 
 end
