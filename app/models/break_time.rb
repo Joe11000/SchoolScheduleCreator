68efaa87e30675d21cfa6e5c8251c9beb1edac51
Joe <JoeNoonan27@gmail.com>
2014-02-04 15:34:17 -0600

    removed files

--- app/models/break_time.rb
+++
@@ -1,8 +0,0 @@
-class BreakTime < ActiveRecord::Base
-	attr_accessible :start_time, :end_time, :confirmed
-
-	belongs_to :teacher
-
-	validates  :start_time, presence: true
-  validates  :end_time,   presence: true
-end
